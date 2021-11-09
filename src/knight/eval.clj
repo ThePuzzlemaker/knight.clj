66a89ef805567e6fe88802833db387c548fafadc
ThePuzzlemaker <tpzker@thepuzzlemaker.info>
2021-11-09 15:54:25 -0600

    Binary funcs

--- src/knight/eval.clj
+++ src/knight/eval.clj
@@ -6,7 +6,9 @@
   (:import  [knight.ast Prompt Rand Nop
              Block Eval Call Shell
              Quit Not Length Dump Output Ascii
-             Value UnaryMinus Use]
+             Value UnaryMinus Use Add Sub Mul
+             Div Rem Pow Lt Gt Eq And Or
+             Semi Assign While]
             [knight.data KnBlock KnString
              KnNumber KnBoolean KnNull KnIdent]))
 
@@ -123,6 +125,58 @@
                         (data/->KnString
                          (slurp (.string string))))
                        ctx))
+
+(kn-create-fn Add [[e1 :unchanged] [e2 :coerced]] [this ctx]
+              (condp instance? e1
+                KnNumber (data/->KnNumber (+ (.inner e1) (.inner e2)))
+                KnString (data/->KnString (str (.inner e1) (.inner e2)))
+                (throw (IllegalArgumentException.
+                        (str "Expected a string or a number, found a `"
+                        (type e1) "`.")))))
+(kn-create-fn Sub [[e1 :number] [e2 :number]] [this ctx]
+              (data/->KnNumber (- (.inner e1) (.inner e2))))
+(kn-create-fn Mul [[e1 :unchanged] [e2 :number]] [this ctx]
+              (condp instance? e1
+                KnNumber (data/->KnNumber (* (.inner e1) (.inner e2)))
+                KnString (data/->KnString (apply str (repeat (.inner e2) (.inner e1))))
+                (throw (IllegalArgumentException.
+                        (str "Expected a string or a number, found a `"
+                             (type e1) "`.")))))
+(kn-create-fn Div [[e1 :number] [e2 :number]] [this ctx]
+              (data/->KnNumber (quot (.inner e1) (.inner e2))))
+(kn-create-fn Rem [[e1 :number] [e2 :number]] [this ctx]
+              (data/->KnNumber (rem (.inner e1) (.inner e2))))
+(kn-create-fn Pow [[e1 :number] [e2 :number]] [this ctx]
+              (data/->KnNumber (int (Math/pow (.inner e1) (.inner e2)))))
+(kn-create-fn Lt [[e1 :unchanged] [e2 :coerced]] [this ctx]
+              (condp instance? e1
+                KnNumber (data/->KnBoolean (< (.inner e1) (.inner e2)))
+                KnString (data/->KnBoolean (= -1 (compare (.inner e1) (.inner e2))))
+                KnBoolean (data/->KnBoolean (and (not (.inner e1)) (.inner e2)))
+                (throw (IllegalArgumentException.
+                        (str "Expected a string, number, or boolean, found a `"
+                             (type e1) "`.")))))
+(kn-create-fn Gt [[e1 :unchanged] [e2 :coerced]] [this ctx]
+              (kn-eval (ast/->Lt e2 e1) ctx))
+(kn-create-fn Eq [[e1 :unchanged] [e2 :unchanged]] [this ctx]
+              (data/->KnBoolean (= e1 e2)))
+(kn-create-fn And [[e1 :unchanged] [e2 :unevaluated]] [this ctx]
+              (if (.inner (data/to-boolean e1))
+                (kn-eval e2 ctx) e1))
+(kn-create-fn Or [[e1 :unchanged] [e2 :unevaluated]] [this ctx]
+              (if (.inner (data/to-boolean e1))
+                e1 (kn-eval e2 ctx)))
+(kn-create-fn Semi [[e1 :unchanged] [e2 :unchanged]] [this ctx] e2)
+(kn-create-fn Assign [[ident :unevaluated] [expr :unchanged]] [this ctx]
+              (do (if (instance? KnIdent ident)
+                    (ctx-put ctx (.inner ident) expr)
+                    (ctx-put ctx (.inner (data/to-string
+                                          (kn-eval ident ctx))) expr))
+                  (data/->KnNull)))
+(kn-create-fn While [[test :unevaluated] [expr :unevaluated]] [this ctx]
+              (do (while (.inner (data/to-boolean (kn-eval test ctx)))
+                    (kn-eval expr ctx))
+                  (data/->KnNull)))
 
 (extend-protocol Evalable
   KnNull (kn-eval-step [this, _] this)
